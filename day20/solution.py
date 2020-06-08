3044537e093ca4469d4e27287233b865f473b936
mingyuchoo <mingyuchoo@gmail.com>
2020-06-09 05:19:54 +0900

    modified

--- day20/solution.py
+++ day20/solution.py
@@ -1,23 +1,58 @@
 class Node:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, target):
+        self.data = target
         self.next = None
 
 
 class LinkedList:
     def __init__(self):
-        dummy = Node("dummy")
-        self.head = dummy
-        self.tail = dummy
-        self.before = None
-        self.current = None
+        self.head = None
 
-    def after(self):
-        self.current = self.head
-        print('after >> ', self.current.data)
+    def append(self, target) -> Node:
+        new_node = Node(target)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+        return self.head
 
-    def append(self, data):
-        new_node = Node(data)
-        self.tail.next = new_node
-        self.tail = new_node
+    def traversal(self) -> list:
+        result = []
+        current = self.head
+        while current:
+            result.append(current.data)
+            current = current.next
+        return result
 
+    def find(self, target) -> bool:
+        result = False
+        current = self.head
+        while current:
+            if current.data == target:
+                return True
+            current = current.next
+        return result
+
+    def delete(self, target):
+        current = self.head
+        if current.data == target:
+            self.head = current.next
+            return current
+        else:
+            while current:
+                if current.next.data == target:
+                    next_next_node = current.next.next
+                    current.next = next_next_node
+                    return current.next
+                current = current.next
+
+    def sort(self):
+        current = self.head
+        next_node = current.next
+        if current.data > next_node.data:
+            current.next = next_node.next
+            next_node.next = current
+            self.head = next_node
--- day20/solution_test.py
+++ day20/solution_test.py
@@ -1,13 +1,30 @@
 import unittest
-
+import string
 from day20.solution import LinkedList
 
 
 class MyTestCase(unittest.TestCase):
+
     def test_something(self):
         l = LinkedList()
-        l.after()
+        data = reversed(string.ascii_uppercase)
+        for i in data:
+            l.append(i)
+        print(l.traversal())
+        self.assertEqual(True, l.find('Z'))
+        self.assertEqual(False, l.find('Hello'))
 
+        l.delete('Z')
+        print(l.traversal())
+
+        l.delete('W')
+        print(l.traversal())
+
+        l.delete('A')
+        print(l.traversal())
+
+        l.sort()
+        print(l.traversal())
 
 if __name__ == '__main__':
     unittest.main()

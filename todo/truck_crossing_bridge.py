2397bb1fc0d329cec3cefc5f5c28600752cdbd4b
mingyuchoo <mingyuchoo@gmail.com>
2020-06-22 20:09:39 +0900

    modified

--- todo/truck_crossing_bridge.py
+++
@@ -1,9 +0,0 @@
-def solution(bridge_length, weight, truck_weights):
-    answer = 0
-    return answer
-
-
-if __name__ == "__main__":
-    print(8 == solution(2, 10, [7, 4, 5, 6]))
-    print(101 == solution(100, 100, [10]))
-    print(110 == solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]))

74f3637472abaede28415059eebd188cd26e8825
mingyuchoo <mingyuchoo@gmail.com>
2020-08-24 03:46:52 +0900

    modified

---
+++ day28_bfs/solution.py
@@ -0,0 +1,26 @@
+# https://cyc1am3n.github.io/2019/04/26/bfs_dfs_with_python.html
+#
+from collections import deque
+
+graph_list = {1: set([3, 4]),
+              2: set([3, 4, 5]),
+              3: set([1, 5]),
+              4: set([1]),
+              5: set([2, 6]),
+              6: set([3, 5])}
+root_node = 1
+
+
+def BFS_with_adj_list(graph, root):
+    visited = []
+    queue = deque([root])
+
+    while queue:
+        n = queue.popleft()
+        if n not in visited:
+            visited.append(n)
+            queue += graph[n] - set(visited)
+    return visited
+
+
+print(BFS_with_adj_list(graph_list, root_node))
